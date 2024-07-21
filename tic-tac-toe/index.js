b18622a2f356aaabbe0d47d3af1f0195e0ca12f2
zjedzonyy <artompor@gmail.com>
2024-07-21 17:56:21 +0200

    Add new folder with initial project structure and initialState function

---
+++ tic-tac-toe/index.js
@@ -0,0 +1,36 @@
+function game() {
+    const X = "X";
+    const O = "O";
+    const EMPTY  = null;
+
+    // initial state function
+    function initialState(){
+        return [
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]
+            ];
+    }
+
+    console.log(initialState());
+}
+
+game();
+
+
+// player function
+
+
+// action function
+
+// result function
+
+
+//winner function
+
+
+//termina function
+
+
+//utility function
+
