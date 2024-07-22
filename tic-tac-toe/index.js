7a278199c9922d81cb48c15d2a8132b7e33056ea
zjedzonyy <artompor@gmail.com>
2024-07-22 17:23:49 +0200

    Add functions to determine the next player and available actions

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,4 +1,4 @@
-function game() {
+function gameBoard() {
     const X = "X";
     const O = "O";
     const EMPTY  = null;
@@ -12,16 +12,53 @@ function game() {
             ];
     }
 
-    console.log(initialState());
+    return initialState();
 }
 
-game();
+const board = gameBoard();
+
+const player = () => {
+    let countx = 0;
+    let counto = 0;
+    
+    for (let i = 0; i <= 2; i++) {
+        for (let j = 0; j <= 2; j++) {
+            if (board[i][j] === "X") {
+                countx++;
+            } else if (board[i][j] === "O") {
+                counto++;
+            }
+        }
+    }
+
+    if (counto >= countx) {
+        return "X";
+    } else if (counto < countx) {
+        return "O";
+    }
+}
+
+const action = () => {
+    const mySet = new Set();
+
+    for (let i = 0; i <= 2; i++) {
+        for (let j = 0; j <= 2; j++) {
+            // console.log(board[i][j]);
+            if (board[i][j] === null) {
+                mySet.add(`${i}, ${j}`);
+                // console.log(`Added (${i},${j}) to mySet`);
+            }
+        }
+    }
+}
+
+player();
 
 
 // player function
 
 
-// action function
+// action function + check if it's valid
 
 // result function
 
