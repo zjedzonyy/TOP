558580dec99fd1a5826e61d3ed84f233fe3eb5ce
zjedzonyy <artompor@gmail.com>
2024-07-23 20:03:01 +0200

    Implement game logic and winner checking
    - Add makeMove function to prompt user for a move and update the board
    - Add arraysEqual function to compare arrays for move validation
    - Implement checkWinner function to check for horizontal and vertical wins
    - Update playGame function to handle game loop until a winner is found

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -15,8 +15,9 @@ function gameBoard() {
     return initialState();
 }
 
-const board = gameBoard();
+let board = gameBoard();
 
+// return whose next turn is, "X" or "O"
 const player = () => {
     let countx = 0;
     let counto = 0;
@@ -38,6 +39,7 @@ const player = () => {
     }
 }
 
+// return set of available actions
 const action = () => {
     const mySet = new Set();
 
@@ -45,29 +47,120 @@ const action = () => {
         for (let j = 0; j <= 2; j++) {
             // console.log(board[i][j]);
             if (board[i][j] === null) {
-                mySet.add(`${i}, ${j}`);
+                mySet.add([i, j]);
                 // console.log(`Added (${i},${j}) to mySet`);
             }
         }
     }
+    return mySet;
 }
 
-player();
+// function isValidMove(prompt) {
+//     const [x, y] = prompt.split(',').map(Number);
+// }
+
+function arraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+}
+
+// ask for a move and update the table
+const makeMove = () => {
+    const whoseMove = player();
+
+    
+    let move = prompt(`${whoseMove} move, type coordinates [x,y]: `);
+    const moveMade = move.split(',').map(Number);
+    // const moveMade = new Set(moveArray);
+
+    console.log(moveMade);
+    const aveMoves = action();
+    console.log(aveMoves);
+
+    for (const value of aveMoves) {
+        if (arraysEqual(moveMade, value)) {
+            console.log("move is valid");
+            board[moveMade[0]][moveMade[1]] = whoseMove;
+            console.log(board);
+        }
+    }
+    
+}
+
+const checkWinner = () => {
+    // sprawdz poziomo 
+    let countx = 0;
+    let counto = 0;
+
+    for (let i = 0; i <= 2; i++) {
+        for (let j = 0; j <= 2; j++) {
+            if (board[i][j] === "X") {
+                countx++;
+                console.log("Punkt dla X");
+            } else if (board[i][j] === "O"){
+                counto++;
+                console.log("punkt dla O");
+            }
+        if (countx === 3) {
+            console.log("Wygral X poziomo")
+            return "X";
+        } else if (counto === 3) {
+            console.log("Wygral O poziomo")
+            return "O";
+        }
+        }
+        countx = 0;
+        counto = 0;
+
+    }
+
+    // sprawdz pionowo 
+    for (let i = 0; i <= 2; i++) {
+        for (let j = 0; j <= 2; j++) {
+            if (board[j][i] === "X") {
+                countx++;
+                console.log("Punkt dla X");
+            } else if (board[j][i] === "O"){
+                counto++;
+                console.log("punkt dla O");
+            }
+        if (countx === 3) {
+            console.log("Wygral X pionowo");
+            return "X";
+        } else if (counto === 3) {
+            console.log("Wygral O pionowo");
+            return "O";
+        }
+        }
+        countx = 0;
+        counto = 0;
+
+    }
+    // sprawdz po skosie 
+    if (board[0][0] == board[1][1] == board[2][2] != null)
 
 
-// player function
+    // brak zwyciezcy 
+    return null;
+
+    
+}
+
+const playGame = () => {
+    let winner = checkWinner()
+    console.log(winner);
+    while (winner === null) {
+        makeMove();
+        winner = checkWinner();
+    }
+    console.log(winner);
+}
+
+playGame();
 
 
-// action function + check if it's valid
-
-// result function
-
-
-//winner function
-
-
-//termina function
-
-
-//utility function
-
+// TODO: zaimplementowanie sprawdzenia remisu (czyli brak avMoves i winner === null)
+// TODO: poprawić co ktora funkcja zwraca i dopiescic kod
